8b3dba82628c8aad776328c24e708bb0a317b489
Hoorain-Nadeem <hoorain2192010@gmail.com>
2026-09-18 21:58:32 +0500

    Update profile.jsx

--- Frontened/mern-project/src/profile.jsx
+++ Frontened/mern-project/src/profile.jsx
@@ -16,7 +16,7 @@ export default function Profile() {
       setLoading(true);
 
       const res = await axios.get(
-        "http://localhost:8080/auth/profile",
+        "https://mern-project-ten-lac.vercel.app/auth/profile",
         {
           withCredentials: true,
         }
