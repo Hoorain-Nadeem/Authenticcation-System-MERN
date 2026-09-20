a902ba955ebd8afb15da167f78f0ba0510ade317
Hoorain-Nadeem <hoorain2192010@gmail.com>
2026-09-20 20:03:22 +0500

    Update index.js

--- Backened/index.js
+++ Backened/index.js
@@ -7,7 +7,7 @@ const authRouter = require("./routes/authRoutes");
 app.use(express.json())
 app.use(
   cors({
-    origin: "https://authenticationsystem-sigma.vercel.app/",
+    origin: "https://authenticationsystem-sigma.vercel.app",
     credentials: true,
   })
 );
